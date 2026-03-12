3d7947e99995fb6af619ff92303d0318b98e6e27
DoPhuongAnh <25112012@st.vju.ac.vn>
2026-03-12 22:08:37 +0700

    update exercise

---
+++ exercise 5.1.py
@@ -0,0 +1,10 @@
+seconds = 42 * 60 +42
+print(seconds)
+# question 2
+miles = 10 / 1.61
+print(miles)
+# question 3
+time_seconds = 42 * 60 + 42
+miles = 10 / 1.61
+speed = miles / (time_seconds / 3600)
+print(speed)
